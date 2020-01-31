e28c7a4616b2754c564b0a959a03b3c89b756fdb
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2020-02-01 00:38:48 +0900

    [update]using pcysl5edgo/unitypackage@1.0.1

---
+++ node_modules/unitypackage/CHANGELOG.md
@@ -0,0 +1,19 @@
+## [1.0.1](https://github.com/pCYSl5EDgo/unitypackage/compare/v1.0.0...v1.0.1) (2020-01-30)
+
+
+### Bug Fixes
+
+* add npm package "git-snapshot" ([312ed3c](https://github.com/pCYSl5EDgo/unitypackage/commit/312ed3c4632e26eea65d90903484aa129dae6217))
+
+# 1.0.0 (2020-01-30)
+
+
+### Bug Fixes
+
+* add npm repositories ([ab384f6](https://github.com/pCYSl5EDgo/unitypackage/commit/ab384f6e48daa9559fb2b3a85586aa05fffb274f))
+* npm ci ([809a263](https://github.com/pCYSl5EDgo/unitypackage/commit/809a26329fa3d253a9b492f5b05aaa79e70f1a9b))
+
+
+### Features
+
+* Initial Commit ([c0527db](https://github.com/pCYSl5EDgo/unitypackage/commit/c0527dbc0d119c9510b8d21a6f2c9a071b729078))
--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,6 @@
 import { getInput, info } from '@actions/core';
-import { mkdirP, cp, rmRF, mv } from '@actions/io';
-import { exec } from '@actions/exec';
-import { execSync } from 'child_process';
-import { safeLoad } from 'js-yaml';
-import { readFile, writeFile } from 'fs';
-import { join, basename } from 'path';
-import { tmpdir } from 'os';
-
-interface AssetMetaData {
-    guid: string;
-    folderAsset: 'yes' | 'no' | undefined
-}
-
-const MakeTGZ = async (tmpFolder: string, output: string) => {
-    info("\n\ntmpFolder : " + tmpFolder + "\noutput : " + output);
-    const archtemp = join(tmpdir(), "archtemp.tar");
-    execSync('cd ' + tmpFolder + "\ntar -cf ../../archtemp.tar ./");
-    await exec('gzip -f ' + archtemp);
-    await mv(archtemp + ".gz", output);
-    await rmRF(tmpFolder);
-};
-
-const CreateOneAssetFolder = (metaFileRelativePathWithExtension: string, projectRoot: string, tmpFolder: string, index: number, output: string, processHasDone: boolean[]) => {
-    const metaFileAbsolutePath = join(projectRoot, metaFileRelativePathWithExtension);
-    readFile(metaFileAbsolutePath, { encoding: "utf-8" }, async (err, data) => {
-        if (err) {
-            throw err;
-        }
-        const metaDatum: AssetMetaData = safeLoad(data);
-        const guid = metaDatum.guid;
-        const dir = join(tmpFolder, guid);
-
-        await mkdirP(dir);
-
-        await cp(metaFileAbsolutePath, join(dir, "asset.meta"));
-
-        if (metaDatum.folderAsset !== "yes") {
-            const assetFileAbsolutePath = metaFileAbsolutePath.substr(0, metaFileAbsolutePath.length - 5);
-            await cp(assetFileAbsolutePath, join(dir, "asset"));
-        }
-
-        const assetFileRelativePath = metaFileRelativePathWithExtension.substr(0, metaFileRelativePathWithExtension.length - 5);
-        writeFile(join(dir, "pathname"), assetFileRelativePath, async () => {
-            processHasDone[index] = true;
-            if (processHasDone.indexOf(false) === -1)
-                await MakeTGZ(tmpFolder, output);
-        });
-    });
-};
-
-const ProcessMetaFiles = (metaFiles: string[], projectRoot: string, tmpFolder: string, output: string, processHasDone: boolean[]) => {
-    metaFiles.forEach((metaFilePath, index, _) => {
-        CreateOneAssetFolder(metaFilePath, projectRoot, tmpFolder, index, output, processHasDone);
-    });
-};
+import create from 'unitypackage';
+import { readFile } from 'fs';
 
 const IsNotNullOrWhiteSpace = (value: string) => value && value.trim();
 
@@ -64,8 +11,6 @@ const Split = (linesConcat: string) => {
 
 const Run = () => {
     const output = getInput("package-path", { required: true });
-    let name = basename(output);
-    name = name.substr(0, name.length - 13);
     const projectFolder = getInput("project-folder", { required: false }) ?? "./";
 
     const includeFilesPath = getInput("include-files", { required: true });
@@ -73,13 +18,8 @@ const Run = () => {
         if (err) {
             throw err;
         }
-        const tmpFolder = join(tmpdir(), name, "archtemp");
-        await mkdirP(tmpFolder);
-        info("include-files\n\n" + data);
         const metaFiles = Split(data);
-        const processHasDone = new Array(metaFiles.length);
-        processHasDone.fill(false);
-        ProcessMetaFiles(metaFiles, projectFolder, tmpFolder, output, processHasDone);
+        create(metaFiles, projectFolder, output, info);
     });
 };
 
