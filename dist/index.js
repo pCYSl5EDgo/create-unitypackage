dc41e23725c5fe396657ca6d6687df90d7870a46
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2024-01-02 22:16:37 +0900

    support working directory

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,19 @@
-import { getInput, info } from '@actions/core';
-import create from 'unitypackage';
-import { readFile } from 'node:fs/promises';
+import { getInput, info } from "@actions/core";
+import create from "unitypackage";
+import { readFile } from "node:fs/promises";
+import { chdir } from "node:process";
 const IsNotNullOrWhiteSpace = (value) => value && value.trim();
 const Split = (linesConcat) => {
     const splits = linesConcat.split(/\r\n|\n|\r/);
     return splits.filter(IsNotNullOrWhiteSpace);
 };
-const Run = async () => {
-    const output = getInput("package-path", { required: true });
-    const projectFolder = getInput("project-folder", { required: false }) ?? "./";
-    const includeFilesPath = getInput("include-files", { required: true });
-    const data = await readFile(includeFilesPath, { encoding: "utf-8" });
-    const metaFiles = Split(data);
-    await create(metaFiles, projectFolder, output, info);
-};
-await Run();
+const workingFolder = getInput("working-folder", { required: false });
+const output = getInput("package-path", { required: true });
+const projectFolder = getInput("project-folder", { required: false }) ?? "./";
+const includeFilesPath = getInput("include-files", { required: true });
+if (workingFolder != null) {
+    chdir(workingFolder);
+}
+const data = await readFile(includeFilesPath, { encoding: "utf-8" });
+const metaFiles = Split(data);
+await create(metaFiles, projectFolder, output, info);
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
-import { getInput, info } from '@actions/core';
-import create from 'unitypackage';
-import { readFile } from 'node:fs/promises';
+import { getInput, info } from "@actions/core";
+import create from "unitypackage";
+import { readFile } from "node:fs/promises";
+import { chdir } from "node:process";
 
 const IsNotNullOrWhiteSpace = (value: string) => value && value.trim();
 
 const Split = (linesConcat: string) => {
-    const splits = linesConcat.split(/\r\n|\n|\r/);
-    return splits.filter(IsNotNullOrWhiteSpace);
+  const splits = linesConcat.split(/\r\n|\n|\r/);
+  return splits.filter(IsNotNullOrWhiteSpace);
 };
 
+const workingFolder = getInput("working-folder", { required: false });
 const output = getInput("package-path", { required: true });
 const projectFolder = getInput("project-folder", { required: false }) ?? "./";
-
 const includeFilesPath = getInput("include-files", { required: true });
+
+if (workingFolder != null) {
+  chdir(workingFolder);
+}
+
 const data = await readFile(includeFilesPath, { encoding: "utf-8" });
 const metaFiles = Split(data);
 await create(metaFiles, projectFolder, output, info);
